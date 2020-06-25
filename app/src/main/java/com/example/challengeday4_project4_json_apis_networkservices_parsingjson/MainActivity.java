e2df673d7158630eea5d8c47a7316aa00bdab1bc
amansharma-dev <amansharma.11@hotmail.com>
2020-06-25 22:26:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/challengeday4_project4_json_apis_networkservices_parsingjson/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.challengeday4_project4_json_apis_networkservices_parsingjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
