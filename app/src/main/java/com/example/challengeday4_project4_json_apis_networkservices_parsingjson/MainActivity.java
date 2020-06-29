b227635fe20bd996ffb4640727b66a38143834dd
amansharma-dev <amansharma.11@hotmail.com>
2020-06-30 01:37:19 +0530

    commit

--- app/src/main/java/com/example/challengeday4_project4_json_apis_networkservices_parsingjson/MainActivity.java
+++ app/src/main/java/com/example/challengeday4_project4_json_apis_networkservices_parsingjson/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.challengeday4_project4_json_apis_networkservices_parsingjson
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+//  object :  fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+    public static final String TAG = "MainActivity";
+    private RequestQueue requestQueue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos/1", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d(TAG, "onResponse: "+"Title: "+response.getString("title")+", Id: "+response.getInt("id")+", Completed: "+response.getBoolean("completed"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: "+error.getMessage());
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
